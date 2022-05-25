bb11228baffd75ed184c1ab51fac348dc93d2987
Anderson Nunes <anderson.nunes1988@gmail.com>
2022-05-25 08:55:14 -0300

    Step 02 - Solution - onClickListener

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -2,17 +2,17 @@ package com.example.android.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TODO (02) Use findViewById to get a reference to the button
-        // and assign it to an immutable variable called rollButton
-
-        // TODO (03) set the OnClickListener for the button
-
-        // TODO (04) Show a Toast when the OnClickListener is called
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
